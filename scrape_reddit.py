f78f4f214d1083f781f166d817e187a7b886ec89
Jeet Bardapurkar <34263533+jeetb@users.noreply.github.com>
2025-06-09 13:08:58 -0400

    Add files via upload

--- scrape_reddit.py
+++ scrape_reddit.py
@@ -3,10 +3,18 @@ import pandas as pd
 from datetime import datetime
 import os
 
+client_id = os.environ.get("REDDIT_CLIENT_ID")
+client_secret = os.environ.get("REDDIT_CLIENT_SECRET")
+user_agent = "civil_unrest_tracker:v1.0 (by u/can_only_comment_OK)"  # Use your real or throwaway Reddit username
+
+if not client_id or not client_secret:
+    raise ValueError("Reddit credentials not found in environment variables")
+
 reddit = praw.Reddit(
-    client_id=os.environ.get("REDDIT_CLIENT_ID"),
-    client_secret=os.environ.get("REDDIT_CLIENT_SECRET"),
-    user_agent='civil_unrest_tracker:v1.0 (by u/can_only_comment_OK)'
+    client_id=client_id,
+    client_secret=client_secret,
+    user_agent=user_agent,
+    check_for_async=False
 )
 
 subreddits = ['news', 'politics', 'nyc', 'Portland', 'LosAngeles', 'Chicago', 'Seattle', 'BlackLivesMatter']
